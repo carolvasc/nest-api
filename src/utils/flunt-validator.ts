ccd8312054703a99a64fa9aa0d49c58cad81c432
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-08 15:26:59 -0300

    Criação do FluntValidator para as validações básicas dos campos

---
+++ src/utils/flunt-validator.ts
@@ -0,0 +1,42 @@
+export class FluntValidator {
+  constructor(public errors: any[] = []) {}
+
+  isRequired(value: string, message: string) {
+    if (!value || value.length <= 0) {
+      this.errors.push(message);
+    }
+  }
+
+  hasMinLen(value: string, min: number, message: string) {
+    if (!value || value.length < min) {
+      this.errors.push(message);
+    }
+  }
+
+  hasMaxLen(value: string, max: number, message: string) {
+    if (!value || value.length > max) {
+      this.errors.push(message);
+    }
+  }
+
+  isFixedLen(value: string, len: number, message: string) {
+    if (value.length !== len) {
+      this.errors.push(message);
+    }
+  }
+
+  isEmail(value: string, message: string) {
+    const reg = new RegExp(/^\w+([-+.']\w+)*@\w+([-.\]\w+)*\.\w+([-.]\w+)*$/);
+    if (!reg.test(value)) {
+      this.errors.push(message);
+    }
+  }
+
+  clear() {
+    this.errors = [];
+  }
+
+  isValid() {
+    return this.errors.length === 0;
+  }
+}
