903842eee753a00264de772f0e723ca8cf13974c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-09 16:54:25 -0300

    Setando o nome do database na ConnectionString

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://carolvasc:carolvasc@petshop.avlcr.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://carolvasc:carolvasc@petshop.avlcr.mongodb.net/petshop?retryWrites=true&w=majority',
     ),
     BackofficeModule,
   ],
