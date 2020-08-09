7cf52ee2b112dbbdaf95f271bc2db6a9b3cccd3c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-09 16:23:52 -0300

    Configurações da conexão com o Mongo

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from './controllers/customer/customer.controller';
+import { CustomerSchema } from './schemas/customer.schema';
+import { UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  controllers: [CustomerController]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Customer',
+        schema: CustomerSchema,
+      },
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+  ],
+  controllers: [CustomerController],
 })
 export class BackofficeModule {}
