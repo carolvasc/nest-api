5bda09902e237361b13d4190c3921798b2d49084
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-09 16:55:06 -0300

    Criação do service Customer e persistência do usuário e do cliente

--- src/backoffice/controllers/customer/customer.controller.ts
+++ src/backoffice/controllers/customer/customer.controller.ts
@@ -15,10 +15,14 @@ import { CreateCustomerContract } from 'src/backoffice/contracts/customer.contra
 import { CreateCustomerDTO } from 'src/backoffice/dtos/create-customer-dto';
 import { AccountService } from 'src/backoffice/services/account.service';
 import { User } from 'src/backoffice/models/user.model';
+import { CustomerService } from 'src/backoffice/services/customer.service';
 
 @Controller('v1/customers')
 export class CustomerController {
-  constructor(private readonly accountService: AccountService) {}
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly customerService: CustomerService,
+  ) {}
 
   @Get()
   get() {
@@ -32,10 +36,25 @@ export class CustomerController {
   @Post()
   @UseInterceptors(new ValidatorInterceptor(new CreateCustomerContract()))
   async post(@Body() model: CreateCustomerDTO) {
+    // Criação do usuário
     const user = await this.accountService.create(
       new User(model.document, model.password, true),
     );
-    return new Result('Cliente criado com sucesso!', user, [], true);
+
+    // Criação do cliente
+    const customer = new Customer(
+      model.name,
+      model.document,
+      model.email,
+      null,
+      null,
+      null,
+      null,
+      user,
+    );
+    const response = await this.customerService.create(customer);
+
+    return new Result('Cliente criado com sucesso!', response, [], true);
   }
 
   @Put(':document')
--- src/backoffice/services/account.service.ts
+++ src/backoffice/services/account.service.ts
@@ -5,7 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AccountService {
-  constructor(@InjectModel('User') private readonly model: Model<User>) { }
+  constructor(@InjectModel('User') private readonly model: Model<User>) {}
 
   async create(data: User): Promise<User> {
     const user = new this.model(data);
