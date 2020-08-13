3308ffe3fee1404a0199b7f354b1ad62cfffb46d
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-12 21:04:20 -0300

    Criação dos métodos de CRUD no serviço

--- src/backoffice/services/customer.service.ts
+++ src/backoffice/services/customer.service.ts
@@ -1,7 +1,9 @@
 import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
-import { Customer } from '../models/customer.model';
 import { InjectModel } from '@nestjs/mongoose';
+import { Customer } from '../models/customer.model';
+import { Address } from '../models/address.model';
+import { Pet } from '../models/pet.model';
 
 @Injectable()
 export class CustomerService {
@@ -12,5 +14,69 @@ export class CustomerService {
   async create(data: Customer): Promise<Customer> {
     const customer = new this.model(data);
     return await customer.save();
+  }
+
+  async findAll(): Promise<Customer[]> {
+    return await this.model
+      .find({}, 'name email document')
+      .sort('name')
+      .exec();
+  }
+
+  async find(document: string): Promise<Customer> {
+    return await this.model
+      .find({ document })
+      .populate('user', 'username')
+      .exec();
+  }
+
+  async addBillingAddress(document: string, data: Address): Promise<Customer> {
+    const options = { upsert: true }; // Se não houver endereço ele vai criar um, se existir ele vai atualizar
+    return await this.model.findOneAndUpdate(
+      { document },
+      {
+        $set: {
+          billingAddress: data,
+        },
+      },
+      options,
+    );
+  }
+
+  async addShippingAddress(document: string, data: Address): Promise<Customer> {
+    const options = { upsert: true };
+    return await this.model.findOneAndUpdate(
+      { document },
+      {
+        $set: {
+          shippingAddress: data,
+        },
+      },
+      options,
+    );
+  }
+
+  async createPet(document: string, data: Pet): Promise<Customer> {
+    const options = { upsert: true, new: true };
+    return await this.model.findOneAndUpdate(
+      { document },
+      {
+        $push: {
+          pets: data,
+        },
+      },
+      options,
+    );
+  }
+
+  async updatePet(document: string, id: string, data: Pet): Promise<Customer> {
+    return await this.model.findOneAndUpdate(
+      { document, 'pets._id': id },
+      {
+        $set: {
+          'pets.$': data,
+        },
+      },
+    );
   }
 }
