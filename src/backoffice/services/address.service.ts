ea495bd3709ed09596bf8b19f0b160853f427ee6
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-13 09:25:30 -0300

    Separação das requisições de pets e Adresses em outros serviços

---
+++ src/backoffice/services/address.service.ts
@@ -0,0 +1,45 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Address } from '../models/address.model';
+import { Customer } from '../models/customer.model';
+import { AddressType } from '../enums/address-type.enum';
+
+@Injectable()
+export class AddressService {
+  constructor(
+    @InjectModel('Customer') private readonly model: Model<Customer>,
+  ) {}
+
+  async create(
+    document: string,
+    data: Address,
+    type: AddressType,
+  ): Promise<Customer> {
+    // Se não houver endereço ele vai criar um, se existir ele vai atualizar
+    const options = { upsert: true };
+
+    // Verifica se é endereço de cobrança ou de destino
+    if (type == AddressType.Billing) {
+      return await this.model.findOneAndUpdate(
+        { document },
+        {
+          $set: {
+            billingAddress: data,
+          },
+        },
+        options,
+      );
+    } else {
+      return await this.model.findOneAndUpdate(
+        { document },
+        {
+          $set: {
+            shippingAddress: data,
+          },
+        },
+        options,
+      );
+    }
+  }
+}
