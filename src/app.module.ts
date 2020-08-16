25c2344e2436b8d3747b392c39d7d5191e72da4c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-16 17:34:19 -0300

    Configuração do MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BackofficeModule } from './backoffice/backoffice.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { StoreModule } from './store/store.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://carolvasc:carolvasc@petshop.avlcr.mongodb.net/petshop?retryWrites=true&w=majority',
     ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: '7180',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [AppController],
   providers: [AppService],
