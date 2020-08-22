a96b746322eab10ec8aa8dea6db80e821c8a551d
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-21 23:00:23 -0300

    Reorganização do BackofficeModule

--- src/backoffice/backoffice.module.ts
+++ src/backoffice/backoffice.module.ts
@@ -1,17 +1,34 @@
+// Modules
 import { Module } from '@nestjs/common';
-import { PetController } from './controllers/pet/pet.controller';
-import { AddressController } from './controllers/address/address.controller';
-import { CustomerController } from './controllers/customer/customer.controller';
-import { CustomerSchema } from './schemas/customer.schema';
-import { UserSchema } from './schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+// Schemas
+import { UserSchema } from './schemas/user.schema';
+import { CustomerSchema } from './schemas/customer.schema';
+// Services
 import { PetService } from './services/pet.service';
 import { AddressService } from './services/address.service';
 import { AccountService } from './services/account.service';
 import { CustomerService } from './services/customer.service';
+import { AuthService } from 'src/shared/services/auth/auth.service';
+// Controllers
+import { PetController } from './controllers/pet/pet.controller';
+import { AddressController } from './controllers/address/address.controller';
+import { CustomerController } from './controllers/customer/customer.controller';
+import { AccountController } from './controllers/account/account.controller';
+// Strategies
+import { JwtStrategy } from 'src/shared/strategies/jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secretOrPrivateKey: '54147f5ce0d2',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
     MongooseModule.forFeature([
       {
         name: 'Customer',
@@ -23,7 +40,19 @@ import { CustomerService } from './services/customer.service';
       },
     ]),
   ],
-  controllers: [CustomerController, AddressController, PetController],
-  providers: [AccountService, CustomerService, AddressService, PetService],
+  controllers: [
+    CustomerController,
+    AddressController,
+    PetController,
+    AccountController,
+  ],
+  providers: [
+    AccountService,
+    CustomerService,
+    AddressService,
+    PetService,
+    AuthService,
+    JwtStrategy,
+  ],
 })
 export class BackofficeModule {}
