664f599772e33938ee7f335b68487c4ba162216e
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-22 18:44:02 -0300

    AccountController

--- src/backoffice/controllers/account/account.controller.ts
+++ src/backoffice/controllers/account/account.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthService } from 'src/shared/services/auth/auth.service';
+import { JwtAuthGuard } from 'src/shared/guards/auth.guard';
+import { RoleInterceptor } from 'src/interceptors/role.interceptor';
 
 @Controller('v1/accounts')
 export class AccountController {
-  constructor() {}
+  constructor(private authService: AuthService) {}
+
+  @Post('')
+  async createtoken(): Promise<any> {
+    return await this.authService.createToken();
+  }
 
   @Get('')
-  @UseGuards(AuthGuard())
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(new RoleInterceptor(['client']))
   findAll() {
     return [];
   }
