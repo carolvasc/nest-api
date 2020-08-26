81d024e52a2746b4dde40f2188746c0b4974e002
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-25 21:58:50 -0300

    Adicionando compreensão nas requisições

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 import { AppModule } from './app.module';
 import { CustomLoggerService } from './shared/services/custom-logger/custom-logger.service';
 
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLoggerService(),
   });
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
