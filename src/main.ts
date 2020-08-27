cedcde2897b20166d808df479e52498955de775c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-26 22:00:08 -0300

    Documentando a API com o Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as compression from 'compression';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CustomLoggerService } from './shared/services/custom-logger/custom-logger.service';
 
@@ -8,6 +9,18 @@ async function bootstrap() {
     logger: new CustomLoggerService(),
   });
   app.use(compression());
+
+  // Open API (Swagger)
+  const options = new DocumentBuilder()
+    .setTitle('Petshop API')
+    .setDescription('API do curso 7180 do Balta.io')
+    .setVersion('1.0.0')
+    .addTag('petshop')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
