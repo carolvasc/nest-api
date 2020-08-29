27655b8c508ad8855d1f6077dc008994beaad386
Carol Vasconcelos <cvsouza04@gmail.com>
2020-08-29 14:57:16 -0300

    Ajuste no modulo

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ReportsService } from './reports.service';
+import { ReportsResolver } from './resolvers/report.resolver';
 
-@Module({})
+@Module({
+  providers: [ReportsService, ReportsResolver]
+})
 export class ReportsModule {}
